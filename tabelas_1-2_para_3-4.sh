7422036c603c5de6dbaeaa844647c00613a0338e
elppans <elppansmk@gmail.com>
2025-10-17 17:45:34 -0300

    Add files via upload

---
+++ tabelas_1-2_para_3-4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Este script compara os arquivos 'tabela1.txt' e 'tabela2.txt',
+# identifica as palavras únicas de cada um (removendo as que aparecem em ambos),
+# e salva o resultado no arquivo 'tabela3.txt'.
+# Útil para eliminar duplicatas entre dois conjuntos de dados textuais.
+
+# Remove duplicatas e ordena
+tr ' ' '\n' < tabela1.txt | sort -u > temp1.txt
+tr ' ' '\n' < tabela2.txt | sort -u > temp2.txt
+
+# Compara e remove palavras comuns
+comm -3 temp1.txt temp2.txt | sed 's/^\t//' > tabela3.txt
+comm -3 temp2.txt temp1.txt | sed 's/^\t//' > tabela4.txt
+
+# Limpa arquivos temporários
+rm temp1.txt temp2.txt
+
